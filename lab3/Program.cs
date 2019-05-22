b8522922032121895774a956fc9893722a873be3
juziem <lus0800@mail.ru>
2019-05-22 08:50:45 +0700

    mult

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -10,19 +10,17 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            double a, b, d, e, f, c;
+            double a, b, f;
 
             a = Convert.ToDouble(Console.ReadLine());
             b = Convert.ToDouble(Console.ReadLine());
-            d = a + b;
+           
             f = a * b;
-            e = a / b;
-            c = a - b;
+          
+          
 
 
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
+    
             Console.WriteLine(f);
             Console.ReadKey();
         }
